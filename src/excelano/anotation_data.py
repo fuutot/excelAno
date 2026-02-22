e2fa85107a89b3568cb786c13c8ceb6dbcd9054f
fuutot <153519467+fuutot@users.noreply.github.com>
2026-02-22 23:18:17 +0900

    add: アノテーション対象の列に欠損値がある場合，エラーを発生させる

--- src/excelano/anotation_data.py
+++ src/excelano/anotation_data.py
@@ -1,16 +1,33 @@
 import pandas as pd
 
 
+class MissingValueError(ValueError):
+    """アノテーションデータに欠損値が含まれている場合に発生するエラー"""
+
+    pass
+
+
 class AnotationData(pd.DataFrame):
     @staticmethod
-    def from_excel(file_path, dtype: dict[str, type]) -> "AnotationData":
+    def from_excel(
+        file_path, dtype: dict[str, type], anotated_cols: list[str]
+    ) -> "AnotationData":
         """
         エクセル形式のアノテーションデータを読み込むメソッド
         args:
             file_path: エクセルファイルのパス
             dtype: 列名とデータ型の辞書
+            anotated_cols: アノテーション対象の列名のリスト
         returns:
             AnotationData: 読み込んだアノテーションデータ
         """
-        df = pd.read_excel(file_path, dtype=dtype)
+        # str型など，Noneに対応できない型を指定した場合，エラーになるため，先に読み込んでから型変換する
+        df = pd.read_excel(file_path)
+
+        # アノテーション対象のデータに欠損値がある場合，エラーを発生させる
+        if df[anotated_cols].isnull().any().any():
+            raise MissingValueError("アノテーションデータに欠損値が含まれています。")
+
+        df = df.astype(dtype)
+
         return AnotationData(df)
